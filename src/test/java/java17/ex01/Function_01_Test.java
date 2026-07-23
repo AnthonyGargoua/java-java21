6df8085195bc6da3a4ef79a7f92f041ee8a28982
Anthony GARGOUA <gargouaanthony@hotmail.fr>
2026-07-23 16:49:18 +0200

    03 terminé --> Exo 1 et Exo 2 ok. Manque Exo 3, 4, 5, 6 et 7

--- src/test/java/java17/ex01/Function_01_Test.java
+++ src/test/java/java17/ex01/Function_01_Test.java
@@ -22,14 +22,16 @@ public class Function_01_Test {
     // TODO le nom sera de la forme "last_<ENTIER>"
     // TODO l'age sera de la forme "<ENTIER>"
     // TODO le mot de passe sera de la forme "pass_<ENTIER>"
-    private Function<Integer, Person> intToPerson = null;
+ // private Function<Integer, Person> intToPerson = null;
+    private Function<Integer, Person> intToPerson = numero -> new Person("first_" + numero, "last_" + numero, numero,"pass_" + numero); // Je définis une Function qui prend 'numero' (Integer) en entrée et renvoie un 'new Person' avec les textes "first_", "last_", "pass_" concaténés à ce numéro.
     // end::intToPerson[]
 
     @Test
     public void test_intToPerson() throws Exception {
 
         // TODO invoquer la fonction intToPerson avec en paramètre l'entier 10.
-        Person result = null;
+     // Person result = null;
+        Person result = intToPerson.apply(10); // J'exécute la fonction 'intToPerson' en lui passant le nombre 10 grâce à la méthode .apply(10)
 
         assert result.getFirstname().equals("first_10");
         assert result.getLastname().equals("last_10");
@@ -43,7 +45,13 @@ public class Function_01_Test {
     // TODO Compléter la définition de cette fonction
     // TODO la propriété owner est valorisé avec la personne en paramètre
     // TODO la propriété balance est valorisé à 1000
-    private Function<Person, Account> personToAccount = null;
+ // private Function<Person, Account> personToAccount = null;
+    private Function<Person, Account> personToAccount = p -> {
+        Account n = new Account();
+        n.setOwner(p);
+        n.setBalance(1000);
+        return n;
+    }; // Je définis une Function qui prend 'p' (Person) en entrée, crée un compte 'n', lui attribue le propriétaire 'p' et le solde de 1000, puis le renvoie.
     // end::personToAccount[]
 
     @Test
@@ -52,7 +60,8 @@ public class Function_01_Test {
         Person person = new Person("Jules", "France", 10, "pass");
 
         // TODO invoquer la fonction personToAccount
-        Account result = null;
+     // Account result = null;
+        Account result = personToAccount.apply(person); // J'exécute la fonction 'personToAccount' en lui passant l'objet 'person' via la méthode .apply(person)
 
         assert result.getOwner().equals(person);
         assert result.getBalance().equals(1000);
--- src/test/java/java17/ex02/Function_02_Test.java
+++ src/test/java/java17/ex02/Function_02_Test.java
@@ -15,14 +15,23 @@ public class Function_02_Test {
     //  tag::buildAccount[]
     // TODO Compléter la fonction buildAccount
     // TODO la fonction possède 2 paramètres en entrée : une personne et un solde
-    BiFunction<Person, Integer, Account> buildAccount = null;
+ // BiFunction<Person, Integer, Account> buildAccount = null;
+    BiFunction<Person, Integer, Account> buildAccount = (p, i) -> {
+        Account a = new Account();
+        a.setOwner(p);
+        a.setBalance(i);
+        return a;
+};
     //  end::buildAccount[]
 
     @Test
     public void test_build_account() throws Exception {
 
         // TODO invoquer la fonction buildAccount pour que le test soit passant
-        Account account = null;
+     // Account account = null;
+
+        Person person = new Person("John", "France", 80, "pass");
+        Account account = buildAccount.apply(person, 500); // J'exécute la fonction 'buildAccount' en lui passant le nombre 500 grâce à la méthode double .apply(person, 500)
 
         assert account.getBalance().equals(500);
         assert account.getOwner().getFirstname().equals("John");
@@ -30,6 +39,4 @@ public class Function_02_Test {
         assert account.getOwner().getAge().equals(80);
         assert account.getOwner().getPassword().equals("pass");
     }
-
-
 }
